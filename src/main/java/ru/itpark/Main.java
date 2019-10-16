6d21fe051d9adaaf08da8a3307c611744381c67d
AlekseiILitvinov <litvinovai988@gmail.com>
2019-10-17 00:19:46 +0300

    Extracted some constants in Main - only used for testing purposes

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -8,17 +8,22 @@ import ru.itpark.model.SearchParameters;
 import java.util.ArrayList;
 
 public class Main {
+
+    public static final String NEW_SAVINOVO = "Ново-Савиновский";
+    public static final String SOVIET = "Советский";
+    public static final String VOLGA_BY = "Приволжский";
+
     public static void main(String[] args) {
         HouseService service = new HouseService();
 
-        service.addHouse(new House(OfferType.RENT_LONG, ApartmentType.APARTMENT, 1, 10_000, "Ново-Савиновский"));
-        service.addHouse(new House(OfferType.RENT_LONG, ApartmentType.APARTMENT, 2, 15_000, "Ново-Савиновский"));
-        service.addHouse(new House(OfferType.RENT_LONG, ApartmentType.APARTMENT, 1, 17_000, "Советский"));
-        service.addHouse(new House(OfferType.RENT_LONG, ApartmentType.APARTMENT, 3, 27_000, "Приволжский"));
-        service.addHouse(new House(OfferType.RENT_LONG, ApartmentType.APARTMENT, 2, 25_000, "Приволжский"));
-        service.addHouse(new House(OfferType.RENT_SHORT, ApartmentType.APARTMENT, 2, 3_000, "Приволжский"));
-        service.addHouse(new House(OfferType.BUY, ApartmentType.APARTMENT, 2, 3_500_000, "Приволжский"));
-        service.addHouse(new House(OfferType.RENT_SHORT, ApartmentType.HOUSE_SECTION, 2, 13_000, "Приволжский"));
+        service.addHouse(new House(OfferType.RENT_LONG, ApartmentType.APARTMENT, 1, 10_000, NEW_SAVINOVO));
+        service.addHouse(new House(OfferType.RENT_LONG, ApartmentType.APARTMENT, 2, 15_000, NEW_SAVINOVO));
+        service.addHouse(new House(OfferType.RENT_LONG, ApartmentType.APARTMENT, 1, 17_000, SOVIET));
+        service.addHouse(new House(OfferType.RENT_LONG, ApartmentType.APARTMENT, 3, 27_000, VOLGA_BY));
+        service.addHouse(new House(OfferType.RENT_LONG, ApartmentType.APARTMENT, 2, 25_000, VOLGA_BY));
+        service.addHouse(new House(OfferType.RENT_SHORT, ApartmentType.APARTMENT, 2, 3_000, VOLGA_BY));
+        service.addHouse(new House(OfferType.BUY, ApartmentType.APARTMENT, 2, 3_500_000, VOLGA_BY));
+        service.addHouse(new House(OfferType.RENT_SHORT, ApartmentType.HOUSE_SECTION, 2, 13_000, VOLGA_BY));
 
         System.out.println(service.searchByPrice(5_000, 20_000));
         System.out.println(service.searchByDistrict("приволжский, советский"));
